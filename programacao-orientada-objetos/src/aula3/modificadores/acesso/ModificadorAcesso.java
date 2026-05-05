ab5d463c0fdd0b1d46b26a3b64a7d743dfa4824f
Glauber Faria <glauber.de.souza.faria@gmail.com>
2026-05-05 19:52:41 -0300

    rename package

--- programacao-orientada-objetos/src/aula3/modificadores/acesso/ModificadorAcesso.java
+++
@@ -1,8 +0,0 @@
-package aula3.modificadores.acesso;
-
-public class ModificadorAcesso {
-    private String atributo1 = "atributo1";
-    public String atributo2 = "atributo2";
-    protected String atributo3 = "atributo3";
-    String atributo4 = "atributo4";
-}
