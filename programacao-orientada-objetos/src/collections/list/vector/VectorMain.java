c8758d5e472f2edd923fd95dcbc13b52bfba70f3
Glauber Faria <glauber.de.souza.faria@gmail.com>
2026-05-14 08:08:31 -0300

    ˜update˜

---
+++ programacao-orientada-objetos/src/collections/list/vector/VectorMain.java
@@ -0,0 +1,13 @@
+package collections.list.vector;
+
+import java.util.List;
+import java.util.Vector;
+
+public class VectorMain {
+    static void main() {
+        List<Integer> v = new Vector<>();
+        v.add(4);
+        v.add(2);
+        System.out.println(v);
+    }
+}
